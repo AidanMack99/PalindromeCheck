56c23dccfdfe833bb52c4bd8bcede1a0edd1de1c
Aidan Mackintosh <aidan.mackintosh@bgi.gi>
2019-09-03 09:18:58 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IsPalindrome("12");
+            IsPalindrome("11");
+            IsPalindrome("Deleveled");
+            IsPalindrome("A_nn_A");
+            
+
+
+            Console.ReadKey();
+
+             
+
+        }
+
+        static void IsPalindrome(string input)
+            {
+
+
+            string reverse;
+
+            char[] ch = input.ToCharArray();
+
+            Array.Reverse(ch);
+            reverse = new string(ch);
+            bool b = input.Equals(reverse, StringComparison.InvariantCultureIgnoreCase);
+
+   
+            if (b == true)
+            {
+                Console.WriteLine("" + input + " is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("" + input + " is not a palindrome");
+            }
+
+
+          
+        }
+        
+
+
+
+    }
+}
+
